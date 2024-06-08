dd76378b4322a6a52e2d84f367d936aa4bb7d1d8
Toke <mtm201305@gmail.com>
2024-06-08 18:50:13 +0500

    update .gitignore, added untitest ignore folders

---
+++ WebServerTests/Controllers/FormControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServer.Controllers;
+using WebServer.Dtos;
+using WebServer.Interfaces;
+
+namespace WebServerTests.Controllers
+{
+    public class FormControllerTest
+    {
+        private readonly IForms _repository;
+        
+        public FormControllerTest()
+        {
+            _repository = A.Fake<IForms>();
+        }
+
+        [Fact]
+        public async Task GetFormsReturnsOk()
+        {
+            //Arrange
+            var katoid = 123;
+            var forms = A.Fake<Task<List<FormDto>>>();//new List<Form> { / ... / };
+            var formList = A.Fake<Task<List<FormDto>>>();
+            //A.CallTo(() => _repository.GetFormsByKatoId(katoid)).Returns(forms);
+            A.CallTo(() => forms).Returns(formList);
+            var controller = new FormController(_repository);
+            //Act
+            var result = await controller.GetForms(katoid);
+
+            //Assert
+            result.Should().NotBeNull();
+            //Assert.IsType<OkObjectResult>(result);
+            //var okResult = (OkObjectResult)result;
+            //Assert.Equal(forms, okResult.Value);
+        }
+    }
+}
