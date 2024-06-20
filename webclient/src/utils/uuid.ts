4731a5d68463c3012771c7d1579d7eeac6c794e2
saduakassov daniyar <dinovko@gmail.com>
2024-06-20 09:40:23 +0500

    generate UUID without using crypto

--- webclient/src/utils/uuid.ts
+++ webclient/src/utils/uuid.ts
@@ -1,1 +1,26 @@
-export const generateGUID: any = () => crypto.randomUUID();
+// export const generateGUID: any = () => crypto.randomUUID();
+
+export function generateGUID(): string {
+    // Helper function to generate a random number and convert it to a hexadecimal string
+    function randomHexDigit() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+  
+    // Combine random hexadecimal strings to form a UUID
+    return (
+      randomHexDigit() +
+      randomHexDigit() +
+      '-' +
+      randomHexDigit() +
+      '-' +
+      randomHexDigit() +
+      '-' +
+      randomHexDigit() +
+      '-' +
+      randomHexDigit() +
+      randomHexDigit() +
+      randomHexDigit()
+    );
+  }
